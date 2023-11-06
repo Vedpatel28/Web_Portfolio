d218edd54d898c9082b116da2362851707d91c69
ved_patel_28 <130833918+Vedpatel28@users.noreply.github.com>
2023-11-06 14:03:07 +0530

    Update text_utils.dart

--- lib/utils/text_utils.dart
+++ lib/utils/text_utils.dart
@@ -3,51 +3,59 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MyTextStyle {
+  
   static TextStyle extraBold = TextStyle(
     fontWeight: FontWeight.w900,
-    fontFamily: "ubuntu",
     fontSize: 22,
-    color: MyColor.bodyTextColor,
+    color: MyColor.darkBackGround,
     shadows: [
       BoxShadow(
-        offset: const Offset(1.5, 1.2),
-        color: MyColor.bodyTextColor,
+        offset: const Offset(1.2, 0.6),
+        color: MyColor.white,
+      ),
+    ],
+  );
+
+  static TextStyle logoText = TextStyle(
+    fontWeight: FontWeight.w900,
+    fontSize: 32,
+    color: MyColor.darkBackGround,
+    shadows: [
+      BoxShadow(
+        offset: const Offset(1.2, 0.6),
+        color: MyColor.white,
       ),
     ],
   );
 
   static TextStyle titleBold = TextStyle(
     fontWeight: FontWeight.w900,
-    fontFamily: "ubuntu",
-    fontSize: 26,
-    color: MyColor.bodyTextColor,
-    shadows: [
-      BoxShadow(
-        offset: const Offset(1.5, 1.2),
-        color: MyColor.bodyTextColor,
-      ),
-    ],
+    fontSize: 38,
+    color: MyColor.darkBackGround,
   );
 
-  static titleItalic({double size = 60}) {
-    TextStyle(
-      color: MyColor.white,
-      fontWeight: FontWeight.w100,
-      fontSize: size,
-      fontFamily: 'italiana',
-      shadows: [
-        BoxShadow(
-          offset: const Offset(1.5, 1.2),
-          color: MyColor.bodyTextColor,
-        ),
-      ],
-    );
-  }
+
+  static TextStyle smallBold = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 16,
+    color: MyColor.darkBackGround,
+  );
+
+  static TextStyle titleItalic = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 20,
+    color: MyColor.darkBackGround,
+  );
+
+  static TextStyle bgText = TextStyle(
+    fontWeight: FontWeight.w900,
+    fontSize: 35,
+    color: MyColor.darkBackGround,
+  );
 
   static TextStyle normalText = TextStyle(
     fontWeight: FontWeight.bold,
-    fontFamily: "ubuntu",
     fontSize: 16,
-    color: MyColor.bodyTextColor,
+    color: MyColor.darkBackGround,
   );
 }
